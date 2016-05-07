2e3b91f410868210b3c1f955f034f15d56056671
Jose Vitaly <jvitaly_93@hotmail.com>
2016-05-07 13:55:59 -0600

    agregar la pantalla faltante de activos

---
+++ SistemaMJP/Activos.aspx
@@ -0,0 +1,144 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Activos.aspx.cs" Inherits="SistemaMJP.Activos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
+    <asp:UpdatePanel runat="server">
+        <ContentTemplate>
+            <div class="form-horizontal">
+                <h3 class="Encabezado">Ingreso de Activo</h3>
+
+                <div class="form-group">
+                    <label class="col-md-2 control-label">Activo</label>
+
+                    <div class="col-md-10">
+                        <asp:DropDownList id="ListaActivos" runat="server" class="form-control dropdown cmbsize" OnSelectedIndexChanged="revisarActivo" AutoPostBack="true"></asp:DropDownList>
+                    </div>
+                    <div style="display: none;" id="MsjErrorActivo" class="col-md-offset-2" runat="server">
+                        <label class="msjErroneo">Debe elegir un Activo</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                        <label class="col-md-2 control-label">Número de Activo</label>
+
+                        <div class="col-md-10">
+                            <asp:TextBox ID="txtNumActivo" runat="server" placeholder="Número de Activo" class="form-control text-box single-line"></asp:TextBox>
+                        </div>
+                        <div style="display: none;" id="MsjErrorNumActivo" class="col-md-offset-2" runat="server">
+                            <label class="msjErroneo">Debe ingresar un número de Activo</label>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Funcionario Asignado</label>
+
+                        <div class="col-md-10">
+                            <asp:TextBox ID="txtFuncionario" runat="server" placeholder="Funcionario" class="form-control text-box single-line"></asp:TextBox>
+                        </div>
+                        <div style="display: none;" id="MsjErrorFuncionario" class="col-md-offset-2" runat="server">
+                            <label class="msjErroneo">Debe ingresar un funcionario</label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Cédula Funcionario</label>
+
+                        <div class="col-md-10">
+                            <asp:TextBox ID="txtCedula" runat="server" placeholder="Cédula" class="form-control text-box single-line"></asp:TextBox>
+                        </div>
+                        <div style="display: none;" id="MsjErrorCedula" class="col-md-offset-2" runat="server">
+                            <label class="msjErroneo">Debe ingresar una cedula</label>
+                        </div>
+                    </div>
+
+                <div class="form-group">
+                    <label class="col-md-2 control-label">Documento</label>
+
+                    <div class="col-md-10">
+                        <asp:TextBox ID="txtDocumento" runat="server" placeholder="Ej: Cajas" class="form-control text-box single-line"></asp:TextBox>
+                    </div>
+                    <div style="display: none;" id="MsjErrorDocumento" class="col-md-offset-2" runat="server">
+                        <label class="msjErroneo">Debe ingresar un documento</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-2 control-label">Fecha Documento</label>
+                    <div class='input-group date col-md-10' style="max-width: 320px">
+
+                        <asp:TextBox type='text' ID="txtFecha" class="form-control" runat="server"></asp:TextBox>
+
+
+                    </div>
+                    <div style="display: none;" id="MsjErrorFecha" class="col-md-offset-2" runat="server">
+                        <label class="msjErroneo">Debe ingresar una fecha</label>
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="col-md-offset-1 col-md-2 alinearDerecha">
+                        <div class="BotonIngreso">
+                            <asp:Button ID="btnAceptar" class="btn btn-default" runat="server" Text="Aceptar" OnClick="aceptar" />
+                        </div>
+
+                    </div>
+                    <div class="col-md-1">
+                        <div class="BotonIngreso">
+                            <asp:Button ID="btnAyS" class="btn btn-default" runat="server" Text="Aceptar y Salir" OnClick="aceptarYSalir" />
+                        </div>
+
+                    </div>
+                    <div class="col-md-3">
+                        <div class="BotonIngreso">
+                            <asp:Button ID="btnCancelar" class="btn btn-default" runat="server" Text="Cancelar" OnClick="cancelar" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+    <script type="text/javascript">//Convertimos el calendario a español
+        $.datepicker.regional['es'] = {
+            closeText: 'Cerrar',
+            prevText: '<Ant',
+            nextText: 'Sig>',
+            currentText: 'Hoy',
+            monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+            dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+            dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+            weekHeader: 'Sm',
+            dateFormat: 'dd/mm/yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['es']);
+        $(document).ready(function () {
+            $("#<%= txtFecha.ClientID %>").datepicker({
+                showmonth: true,
+                autoSize: true,
+                showAnim: 'slideDown',
+                duration: 'fast'
+            });
+                       
+
+        });
+
+        var prm = Sys.WebForms.PageRequestManager.getInstance();
+
+        prm.add_endRequest(function () {
+            $("#<%= txtFecha.ClientID %>").datepicker({
+                showmonth: true,
+                autoSize: true,
+                showAnim: 'slideDown',
+                duration: 'fast'
+            });            
+
+        });
+
+    </script>
+</asp:Content>
---
+++ SistemaMJP/Activos.aspx.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaMJP
+{
+    public partial class Activos : System.Web.UI.Page
+    {
+        public static bool editar;
+        ControladoraActivos controladora = new ControladoraActivos();
+        Bitacora bitacora = new Bitacora();
+        public static string numActivo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {//Se revisa primero si tiene los permisos para ingresar al sitio
+                string rol = (string)Session["rol"];
+                if (Session["correoInstitucional"] == null)
+                {
+                    Response.Redirect("Ingresar");
+                }
+                else if (!rol.Equals("Inclusion Pedidos") || !rol.Equals("Administrador Almacen"))
+                {
+                    Response.Redirect("MenuPrincipal");
+                }
+
+                else if (Request.UrlReferrer == null)
+                {
+                    Response.Redirect("MenuPrincipal");
+                }
+                else
+                {
+                    llenarListaActivos();
+                    
+                    if (editar)
+                    {
+                        //campos no editables
+                        ListaActivos.Enabled = false;
+                        txtNumActivo.Enabled = false;
+                        llenarDatosActivo();
+                    }
+                }
+            }
+        }
+
+        private void llenarListaActivos() {
+            ListaActivos.Items.Clear();
+            List<string> bodegas = (List<string>)Session["bodegas"];
+            string bodega = bodegas[0];
+            Dictionary<string, int> activos = controladora.getActivos(bodega);
+            ListaActivos.Items.Add("---Elija un Activo---");
+            foreach (var nombreS in activos)
+            {
+                ListaActivos.Items.Add(new ListItem { Text = nombreS.Key, Value = nombreS.Value.ToString() });
+            }
+        
+        }
+
+        //Si se selecciona un Activo el msj de error se esconde
+        protected void revisarActivo(object sender, EventArgs e)
+        {
+            if (ListaActivos.SelectedIndex != 0)
+            {
+                MsjErrorActivo.Style.Add("display", "none");
+            }
+        }
+
+        //Obtiene los datos del producto a editar
+        protected void llenarDatosActivo()
+        {
+            List<string> datos = controladora.obtenerDatosActivo(numActivo);
+            txtNumActivo.Text = datos[0];
+            txtFuncionario.Text = datos[1];
+            txtCedula.Text = datos[2];
+            txtDocumento.Text = datos[3];
+            txtFecha.Text = datos[4];
+            ListaActivos.Items.FindByText(datos[5]).Selected = true;
+
+        }
+
+        //Revisa que los datos proporcionados estén correctos,de ser así los inserta y se mantiene en la página para nuevo ingreso de productos
+        protected void aceptar(object sender, EventArgs e)
+        {
+            
+            if (validar())
+            {//Si todo es valido, entonces procedo a obtener los datos dados por el usuario
+                numActivo = txtNumActivo.Text;
+                DateTime fecha = Convert.ToDateTime(txtFecha.Text);
+                string funcionario = txtFuncionario.Text;
+                string cedula =txtCedula.Text;
+                string documento = txtDocumento.Text;
+                                
+                object[] nuevoActivo = new object[6];
+                nuevoActivo[0] = numActivo;
+                nuevoActivo[1] = fecha;
+                nuevoActivo[2] = funcionario;
+                nuevoActivo[3] = cedula;
+                nuevoActivo[4] = documento;
+                nuevoActivo[5] = Convert.ToInt32(ListaActivos.SelectedValue.ToString());
+                              
+                if (editar)
+                {
+                    controladora.modificarActivo(nuevoActivo);
+                    Response.Redirect("ControlActivos");
+                }
+                else
+                {
+                    int resultado = controladora.agregarActivoFinal(nuevoActivo);
+                    if (resultado == 0) {
+                        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Mensaje de alerta", "alert('El número de activo ya existe')", true);
+
+                    }
+                }
+                }
+
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Mensaje de alerta", "alert('Activo " + ListaActivos.SelectedItem.Text + " agregado con éxito')", true);
+
+                //Al final se limpian los campos
+                txtNumActivo.Text = "";
+                txtFecha.Text = "";
+                txtFuncionario.Text = "";
+                txtCedula.Text = "";
+                txtDocumento.Text = "";
+                ListaActivos.SelectedIndex = 0;
+
+            
+        }
+        //Revisa que los datos proporcionados estén correctos,de ser así los inserta y redirecciona a la página DetallesFactura
+        protected void aceptarYSalir(object sender, EventArgs e)
+        {
+            if (validar())
+            {//Si todo es valido, entonces procedo a obtener los datos dados por el usuario
+                numActivo = txtNumActivo.Text;
+                DateTime fecha = Convert.ToDateTime(txtFecha.Text);
+                string funcionario = txtFuncionario.Text;
+                string cedula = txtCedula.Text;
+                string documento = txtDocumento.Text;
+
+                object[] nuevoActivo = new object[6];
+                nuevoActivo[0] = numActivo;
+                nuevoActivo[1] = fecha;
+                nuevoActivo[2] = funcionario;
+                nuevoActivo[3] = cedula;
+                nuevoActivo[4] = documento;
+                nuevoActivo[5] = Convert.ToInt32(ListaActivos.SelectedValue.ToString());
+
+                if (editar)
+                {
+                    controladora.modificarActivo(nuevoActivo);
+                    
+                }
+                else
+                {
+                    controladora.agregarActivoFinal(nuevoActivo);
+                }
+            }
+
+            Response.Redirect("ControlActivos");
+
+        }
+        protected void cancelar(object sender, EventArgs e)
+        {
+              Response.Redirect("ControlActivos");
+            
+        }
+
+        //Metodo encargado de validar los campos antes de ejecutar una accion
+        protected bool validar()
+        {
+            bool valido = false;
+            string numero = txtNumActivo.Text;
+            string funcionario = txtFuncionario.Text;
+            string cedula = txtCedula.Text;
+            string fecha = txtFecha.Text;
+            string documento = txtDocumento.Text;
+            string producto = ListaActivos.Items[ListaActivos.SelectedIndex].Text;
+            if (producto.Equals("---Elija un Activo---"))
+            {//Ocultar y mostrar mensajes de Error
+                MsjErrorActivo.Style.Add("display", "block");
+                MsjErrorNumActivo.Style.Add("display", "none");
+                MsjErrorCedula.Style.Add("display", "none");
+                MsjErrorFuncionario.Style.Add("display", "none");
+                MsjErrorFecha.Style.Add("display", "none");
+                MsjErrorDocumento.Style.Add("display", "none");
+            }
+            else if (numero.Replace(" ","").Equals(""))
+            {
+                MsjErrorActivo.Style.Add("display", "none");
+                MsjErrorNumActivo.Style.Add("display", "block");
+                MsjErrorCedula.Style.Add("display", "none");
+                MsjErrorFuncionario.Style.Add("display", "none");
+                MsjErrorFecha.Style.Add("display", "none");
+                MsjErrorDocumento.Style.Add("display", "none");
+            }
+            else if (funcionario.Replace(" ","").Equals(""))
+            {
+                MsjErrorActivo.Style.Add("display", "none");
+                MsjErrorNumActivo.Style.Add("display", "none");
+                MsjErrorCedula.Style.Add("display", "none");
+                MsjErrorFuncionario.Style.Add("display", "block");
+                MsjErrorFecha.Style.Add("display", "none");
+                MsjErrorDocumento.Style.Add("display", "none");
+            }
+            else if (cedula.Replace(" ","").Equals(""))
+            {
+                MsjErrorActivo.Style.Add("display", "none");
+                MsjErrorNumActivo.Style.Add("display", "none");
+                MsjErrorCedula.Style.Add("display", "block");
+                MsjErrorFuncionario.Style.Add("display", "none");
+                MsjErrorFecha.Style.Add("display", "none");
+                MsjErrorDocumento.Style.Add("display", "none");
+
+            }
+            else if (documento.Replace(" ","").Equals(""))
+            {
+                MsjErrorActivo.Style.Add("display", "none");
+                MsjErrorNumActivo.Style.Add("display", "none");
+                MsjErrorCedula.Style.Add("display", "none");
+                MsjErrorFuncionario.Style.Add("display", "none");
+                MsjErrorFecha.Style.Add("display", "none");
+                MsjErrorDocumento.Style.Add("display", "block");
+
+            }
+            else if (fecha.Equals(""))
+            {
+                MsjErrorActivo.Style.Add("display", "none");
+                MsjErrorNumActivo.Style.Add("display", "none");
+                MsjErrorCedula.Style.Add("display", "none");
+                MsjErrorFuncionario.Style.Add("display", "none");
+                MsjErrorFecha.Style.Add("display", "block");
+                MsjErrorDocumento.Style.Add("display", "none");
+
+            }
+            else
+            {
+                valido = true;
+            }
+            return valido;
+                
+        }
+    }
+}
